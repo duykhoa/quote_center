bc28fed54120419838cb1fef5a3b39f52b8f8037
Duy Khoa <duykhoa12t@gmail.com>
2019-10-26 11:48:28 +0800

    Use defaultValue to fix warning message

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default class App extends Component {
       <>
         <div className="formFields">
           <label>Start</label>
-          <input value={this.state.start} onInput={(e) => {
+          <input defaultValue={this.state.start} onInput={(e) => {
             const start = e.target.value;
             start > 0 && start < quotes.length && this.setState({ start: start });
           }} type="range" placeholder="start" min={0} max={quotes.length}></input>
@@ -32,7 +32,7 @@ export default class App extends Component {
 
         <div className="formFields">
           <label>End</label>
-          <input value={this.state.end} onInput={(e) => {
+          <input defaultValue={this.state.end} onInput={(e) => {
             const end = e.target.value;
             end > 0 && end <= quotes.length && this.setState({ end: end });
           }} type="range" placeholder="end" min={0} max={quotes.length}></input>
