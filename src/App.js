280d79ce99260ef30756d4970c042ef2b257d4cd
Duy Khoa <duykhoa12t@gmail.com>
2019-11-23 13:32:31 +0800

    Add conversation script

--- src/App.js
+++ src/App.js
@@ -2,72 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import { getQuotes } from './firebase_api/index';
 import 'normalize.css/normalize.css';
-import _ from 'lodash';
+import { createConversationScript } from './ConversationScript';
 
 export default class App extends Component {
   constructor() {
     super();
-    this.state = { quotes: [], firstNotificationText: "", firstButtonEnabled: true, firstButtonClicked: false, firstReplyNotificationText: "", offset: 0, limit: 0, displayQuote1: null };
+    this.state = {quotes: [], actions: ["showQuote"]};
+    this.followUp = this.followUp.bind(this);
   }
 
   componentDidMount() {
     getQuotes({}, snapVal => {
-      this.setState({ quotes: snapVal });
+      this.setState({quotes: snapVal});
     });
   }
 
-  render() {
-    const { offset, limit, quotes, firstNotificationText, firstButtonEnabled, firstButtonClicked, firstReplyNotificationText, displayQuote1 } = this.state;
-    let renderedQuotes = [];
+  followUp(action) {
+    const { actions } = this.state;
 
+    this.setState({
+      actions: [...actions, action]
+    })
+  }
+
+  render() {
+    const { quotes, actions } = this.state;
     return (
       <>
-        <div>
-          <h1>There are many quotes, click until you find the one you like</h1>
-          <div>
-            <button
-              disabled={!firstButtonEnabled}
-              onClick={e => {
-                const offset = _.random(0, quotes.length);
-
-                this.setState({
-                  firstNotificationText: "You actually click this f* button! Ok, here is a quote",
-                  offset: offset, limit: 1,
-                  firstButtonEnabled: false,
-                  firstButtonClicked: true,
-                });
-              }}
-            >click</button>
-          </div>
-
-          <h2>{firstNotificationText}</h2>
-        </div>
-
-        <div>
-          { quotes.slice(offset, offset + limit).map(q => (<q key={q.body}>{q.body}</q>)) }
-
-          {
-            firstButtonClicked &&
-            (<div>
-              <p>Do you like it?</p>
-              <button
-                onClick={ e => {
-                  this.setState({firstReplyNotificationText: "yeah, you like the quote! I will show more then"});
-                }}
-              >yes</button>
-
-              <button
-                onClick={ e => {
-                  this.setState({firstReplyNotificationText: "Why don't you like it? It is Jim Rohn quotes you know?"});
-                }}
-              >no</button>
-            </div>)
-          }
-
-          {
-            firstReplyNotificationText && <p>{firstReplyNotificationText}</p>
-          }
-        </div>
+        { actions.map((action, index) => createConversationScript(quotes, action, index, this.followUp)) }
       </>
     );
   }
---
+++ src/ConversationScript.js
@@ -0,0 +1,135 @@
+import React, { Component, useState } from 'react';
+import _ from 'lodash';
+
+const createConversationScript = (quotes, action, index, followUp) => {
+  const actionTitles = {
+    "showQuote": "Click to see the quote",
+    "showQuoteDislikeFeedback": "Oh! This one you may like",
+    "showQuoteLikeFeedback": "You may also like this one"
+  }
+
+  const title = actionTitles[action];
+
+  // only show view quote button for showQuotes action
+  const withViewQuoteButton = action === "showQuote";
+
+  return (
+    <ConversationScript
+      key={index}
+      title={title}
+      quotes={quotes}
+      withViewQuoteButton={withViewQuoteButton}
+      followUp={{
+        like: () => followUp("showQuoteLikeFeedback"),
+        dislike: () => followUp("showQuoteDislikeFeedback")
+      }}
+    ></ConversationScript>
+  )
+}
+
+class ConversationScript extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showQuoteButtonEnabled: true,
+      displayFeedback: false
+    }
+
+    this.feedbackYes = this.feedbackYes.bind(this);
+    this.feedbackNo = this.feedbackNo.bind(this);
+  }
+
+  feedbackYes() {
+    this.props.followUp.like();
+  }
+
+  feedbackNo() {
+    this.props.followUp.dislike();
+  }
+
+  componentDidMount() {
+    const {quotes, withViewQuoteButton} = this.props;
+    const index = _.random(0, quotes.length);
+    const quote = quotes[index];
+
+    if (!withViewQuoteButton) {
+      const index = _.random(0, quotes.length);
+      const quote = quotes[index];
+      this.setState({ displayQuote: quote, displayFeedback: true });
+    }
+  }
+
+  render() {
+    const {quotes, title, withViewQuoteButton} = this.props;
+    const {displayQuote, displayFeedback, showQuoteButtonEnabled} = this.state;
+    let viewQuoteButton;
+
+    if (withViewQuoteButton) {
+      viewQuoteButton = (
+        <button
+          disabled={!showQuoteButtonEnabled}
+          onClick={e => {
+            const index = _.random(0, quotes.length);
+            const quote = quotes[index];
+
+            this.setState({
+              displayQuote: quote,
+              displayFeedback: true,
+              showQuoteButtonEnabled: false
+            });
+          }}
+        >Click</button>);
+    } else {
+      viewQuoteButton = (<></>);
+    }
+
+    return (
+      <>
+        <h1>{title}</h1>
+
+        <div>
+          { viewQuoteButton }
+        </div>
+
+        {
+          displayQuote && <p>{displayQuote.body}</p>
+        }
+
+        {
+          displayFeedback && (
+            <Feedback
+              feedbackYes={this.feedbackYes}
+              feedbackNo={this.feedbackNo}
+            ></Feedback>
+          )
+        }
+      </>
+    );
+  }
+}
+
+const Feedback = (props) => {
+  const [disable, setDisable] = useState(false);
+
+  return (
+    <>
+      <h3>Do you like it?</h3>
+
+      <button onClick={() => {
+          setDisable(true);
+          props.feedbackYes();
+        }}
+        disabled={disable}
+      >yes</button>
+
+      <button onClick={() => {
+          setDisable(true);
+          props.feedbackNo()
+        }}
+        disabled={disable}
+      >no</button>
+    </>
+  )
+}
+
+export { ConversationScript, createConversationScript };
